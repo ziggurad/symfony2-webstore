75ac7e69fa2b1c5cfb700425431d582fe6f2fec1
Mariusz Kozłowski <mariusz@kozlowski.it>
2015-09-27 22:01:18 +0200

    add and render ProductChoices Form

--- src/WebstoreBundle/Controller/DefaultController.php
+++ src/WebstoreBundle/Controller/DefaultController.php
@@ -6,8 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('WebstoreBundle:Default:index.html.twig', array('name' => $name));
+        $form = $this->createForm('product_choices');
+
+        return $this->render('WebstoreBundle:Default:index.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/WebstoreBundle/Form/Type/ProductChoices.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace WebstoreBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use WebstoreBundle\Entity\ProductRepository;
+
+class ProductChoices extends AbstractType
+{
+    /** @var ProductRepository */
+    private $productRepository;
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'product_choices';
+    }
+
+    public function setProductRepository($productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'products',
+                'entity',
+                array(
+                    'class' => 'WebstoreBundle\Entity\Product',
+                    'choice_label' => 'name',
+                    'required' => true,
+                    'expanded' => true
+                )
+            )
+            ->add('save', 'submit');
+    }
+}
