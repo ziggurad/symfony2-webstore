265178657b6203972b13d2964063272dac85844b
Mariusz Kozłowski <mariusz@kozlowski.it>
2015-09-27 19:08:21 +0200

    install and add DataFixtures

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new WebstoreBundle\WebstoreBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/WebstoreBundle/DataFixtures/ORM/SampleProductsLoad.php
@@ -0,0 +1,35 @@
+<?php
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use WebstoreBundle\Entity\Product;
+
+class SampleProductsLoad implements FixtureInterface
+{
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param \Doctrine\Common\Persistence\ObjectManager $manager
+     */
+    public function load(\Doctrine\Common\Persistence\ObjectManager $manager)
+    {
+        $products = [
+            [
+                'name' => 'Product 1',
+            ],
+            [
+                'name' => 'Product 2',
+            ],
+            [
+                'name' => 'Product 3',
+            ],
+        ];
+
+        foreach ($products as $product) {
+            $entity = new Product();
+            $entity->setName($product['name']);
+            $manager->persist($entity);
+        }
+
+        $manager->flush();
+    }
+}
